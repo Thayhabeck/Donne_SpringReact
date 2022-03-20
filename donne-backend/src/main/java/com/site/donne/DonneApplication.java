fb074a217f5f0a380afe4526000ff588d99e123f
Thayhabeck <thaynahabeck@gmail.com>
2022-03-20 19:40:45 -0300

    projeto cru

---
+++ donne-backend/src/main/java/com/site/donne/DonneApplication.java
@@ -0,0 +1,13 @@
+package com.site.donne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonneApplication.class, args);
+	}
+
+}
---
+++ donne-backend/src/test/java/com/site/donne/DonneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.donne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
